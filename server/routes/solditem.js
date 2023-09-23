c0b44222b0de066472f57d53e22221cf531b794c
Himasnhu-AT <aristech.official@gmail.com>
2023-09-23 10:43:02 +0530

    static landing page with bakend done

--- server/routes/solditem.js
+++ server/routes/solditem.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const SoldItem = require("../models/solditem");
+const SoldItem = require("../models/soldItem");
 
-router.get("/:userId", async (req, res) => {
+
+router.get("/find", async (req, res) => {
   try {
-    const userId = req.params.userId;
-
-    const soldItems = await SoldItem.find({ userId });
-    res.json(soldItems);
+    const userId = req.headers.userid;
+    const Ewaste = req.headers.ewaste;
+    
+    if (userId) {
+      const soldItems = await SoldItem.find({ userId});
+      res.json(soldItems);
+    } else if (Ewaste) {
+      // If only 'userId' is presentß
+      // Query your data based on only 'userId'
+      const soldItems = await SoldItem.find({ Ewaste });
+      res.json(soldItems);
+    }else {
+      // Handle the case where either 'userId' or 'Ewaste' is missing in the header
+      res.status(400).json({ message: "Bad Request: Both 'userId' and 'Ewaste' should be present in the headers" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
+
 
 router.post("/add", async (req, res) => {
   try {
