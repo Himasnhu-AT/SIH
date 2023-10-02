8ac93a73112218f2a2b0d520272d1c58e08bbb90
Himasnhu-AT <aristech.official@gmail.com>
2023-10-02 22:15:09 +0530

    Final build

--- mobile_app/lib/const/global_variable.dart
+++ mobile_app/lib/const/global_variable.dart
@@ -3,7 +3,7 @@
 const user_auth_token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MDIzNGRmYWU1ZWE0MzdjODg4MTNiNCIsImlhdCI6MTY5NDY0MzU2NX0.i1OmAldmVG5qxoI5ZhmXMyT9j2f-lN2N9i6KMRPBW6M";
 
-const server_URL = "http://localhost:3000";
+const server_URL = "https://sih-e-waste-facility-locator.onrender.com";
 const Login_User_API = "$server_URL/api/signin";
 const Verify_user_logged_in = "$server_URL/tokenIsValid";
 const Get_user_data = "$server_URL/api/user";
